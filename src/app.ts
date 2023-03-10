08ec56b3410d0e6fee1b63f92c954ae14e0d8106
maxt0d0nt <maxbox21@hotmail.com>
2023-03-10 13:41:00 +0100

    add my sql 2

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,33 @@
-import express, { Application } from 'express';
-import morgan from 'morgan'
+import express, { Application } from "express";
+import morgan from "morgan";
+
+//Routes
+import IndexRoutes from "./routes/index.routes";
 
 export class App {
+  app: Application;
 
-    app: Application;
+  constructor(private port?: number | string) {
+    this.app = express();
+    this.setting();
+    this.middleware();
+    this.routes();
+  }
 
-    constructor(private port?: number | string){
-        this.app = express();
-        this.setting();
-this.middleware();
-    }
+  setting() {
+    this.app.set("port", this.port || process.env.PORT || 3000);
+  }
 
-    setting() {
-        this.app.set('port', this.port || process.env.PORT || 3000)
-    }
+  middleware() {
+    this.app.use(morgan("dev"));
+  }
 
-    middleware(){
-        this.app.use(morgan('dev'))
-    }
+  routes() {
+    this.app.use(IndexRoutes);
+  }
 
-    async listen() {
-        await this.app.listen(this.app.get('port'));
-        console.log('Server on port', 3000);
-    }
-
+  async listen() {
+    await this.app.listen(this.app.get("port"));
+    console.log("Server on port", 3000);
+  }
 }
